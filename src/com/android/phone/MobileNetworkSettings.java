55b9a7cd172f369cf0e6a7b7445fd2c5fec5c678
jereksel <jereksel@gmail.com>
2016-01-22 21:38:39 +0100

    Fixes cellular network on mako, other devices
    Change-Id: If1e4d4460283d6a531479e28f2d5906eedd1cce9

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -30,6 +30,7 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.AsyncResult;
 import android.os.Bundle;
@@ -629,15 +630,8 @@ public class MobileNetworkSettings extends Activity  {
             mMobileDataPref = (MobileDataPreference) findPreference(BUTTON_MOBILE_DATA_ENABLE_KEY);
             mDataUsagePref = (DataUsagePreference) findPreference(BUTTON_DATA_USAGE_KEY);
 
-            try {
-                Context con = activity.createPackageContext("com.android.systemui", 0);
-                int id = con.getResources().getIdentifier("config_show4GForLTE",
-                        "bool", "com.android.systemui");
-                mShow4GForLTE = con.getResources().getBoolean(id);
-            } catch (PackageManager.NameNotFoundException e) {
-                Log.e(LOG_TAG, "NameNotFoundException for show4GFotLTE");
-                mShow4GForLTE = false;
-            }
+            mShow4GForLTE = Settings.System.getIntForUser(activity.getContentResolver(),
+                                Settings.System.SHOW_LTE_FOURGEE, 0, UserHandle.USER_CURRENT) == 1;
 
             //get UI object references
             PreferenceScreen prefSet = getPreferenceScreen();
