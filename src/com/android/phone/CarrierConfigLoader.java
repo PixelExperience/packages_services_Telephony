190ce6a3dde47ddc31a201ed74e2cb4f66c24502
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-11 01:25:49 +0000

    Revert "Telephony: use CUSTOM_FINGERPRINT to check for system update"
    This reverts commit 790a5bc123aa6b8e3353417575a35d8dd76d8c3c.
    
    Change-Id: I28a997b279824d043cd69ba8e77b61633cbdf214

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -478,16 +478,16 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.CUSTOM_FINGERPRINT.equals(lastFingerprint)) {
+                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
                         log(
                                 "Build fingerprint changed. old: "
                                         + lastFingerprint
                                         + " new: "
-                                        + Build.CUSTOM_FINGERPRINT);
+                                        + Build.FINGERPRINT);
                         clearCachedConfigForPackage(null);
                         sharedPrefs
                                 .edit()
-                                .putString(KEY_FINGERPRINT, Build.CUSTOM_FINGERPRINT)
+                                .putString(KEY_FINGERPRINT, Build.FINGERPRINT)
                                 .apply();
                     }
                     break;
